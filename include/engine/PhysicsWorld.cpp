5a94eed0e193f9fbb14b86d208e763d0b2f76f94
CelesteOvO <1227657366@qq.com>
2023-10-25 18:56:38 +0800

    [basic frame] ① update Class PhysicsWorld, try TODO1

--- include/engine/PhysicsWorld.cpp
+++ include/engine/PhysicsWorld.cpp
@@ -1,6 +1,6 @@
 #include "PhysicsWorld.h"
 
-PhysicsWorld::PhysicsWorld() : m_preStepFunc(nullptr), m_resetFunc(nullptr)
+PhysicsWorld::PhysicsWorld() : _preStepFunc(nullptr), _resetFunc(nullptr)
 {
     physicsWorldData._contactStiffness = 1e6f;
     physicsWorldData._contactDamping = 1e5f;
@@ -34,18 +34,96 @@ void PhysicsWorld::step(float dt) {
     computeInertias();
 
     /// 执行每一步模拟之前的回调函数
-    if( m_preStepFunc )
+    if( _preStepFunc )
     {
-        m_preStepFunc(_bodies);
+        _preStepFunc(_bodies);
     }
 
     /// 3. 检测碰撞，计算接触的雅可比矩阵
-    //_collisionDetect->detectCollision();
-    //_collisionDetect->computeContactJacobians();
+    _collisionDetect->detectCollisions();
+    _collisionDetect->computeContactJacobians();
 
-    /// 更新所有接触的接触刚度和阻尼
+    /// 4. 更新所有接触的接触刚度和阻尼
+    for(auto c : _collisionDetect->_contacts)
+    {
+        c->_contactStiffness = physicsWorldData._contactStiffness;
+        c->_contactDamping = physicsWorldData._contactDamping;
+        c->_frictionCoefficient = physicsWorldData._frictionCoefficient;
+    }
+
+    for(auto b : _bodies)
+    {
+        b->RigidBodyData._linearForce.setZero();
+        b->RigidBodyData._angularConstraintForce.setZero();
+    }
+
+    /// 5. 计算约束力
+    calcConstraintForces(dt);
+
+    /// TODO: 1. 更新刚体的位置和旋转
+    /// 6. 更新刚体的线速度和角速度
+    for(auto b : _bodies)
+    {
+        if(b->RigidBodyData._bodyTypes == STATIC)
+            continue;
+        b->RigidBodyData._linearVelocity += dt * b->RigidBodyData._linearForce / b->RigidBodyData._mass;
+        b->RigidBodyData._angularVelocity += dt * b->RigidBodyData._worldIinv * (b->RigidBodyData._torque - b->RigidBodyData._angularVelocity.cross(b->RigidBodyData._worldI * b->RigidBodyData._angularVelocity));
+        /// TODO: 2. 更新刚体的位置和旋转
+        b->RigidBodyData._position += dt * b->RigidBodyData._linearVelocity;
+        //b->RigidBodyData._orientation += dt * 0.5f * b->RigidBodyData._orientation * b->RigidBodyData._angularVelocity;
+    }
 
 }
+
+void PhysicsWorld::clear() {
+    if( _resetFunc)
+    {
+        _resetFunc();
+    }
+
+    _collisionDetect->clear();
+
+    for(auto b : _bodies)
+    {
+        delete b;
+    }
+    _bodies.clear();
+}
+
+void PhysicsWorld::computeInertias() {
+    for(RigidBody* b : _bodies)
+    {
+        b->updateInertiaMatrix();
+    }
+}
+
+void PhysicsWorld::calcConstraintForces(float dt) const {
+    //_solver->setMaxIter(physicsWorldData._solverIter);
+    //_solver->solve(dt);
+
+    for(const auto c : _collisionDetect->_contacts)
+    {
+        const Eigen::Vector6f f0 = c->_Jacobian0.transpose() * c->_lambda / dt;
+        const Eigen::Vector6f f1 = c->_Jacobian1.transpose() * c->_lambda / dt;
+
+        if (c->_body0->RigidBodyData._bodyTypes != STATIC)
+        {
+            c->_body0->RigidBodyData._linearConstraintForce += f0.head<3>();
+            c->_body0->RigidBodyData._angularConstraintForce += f0.tail<3>();
+        }
+        if (c->_body1->RigidBodyData._bodyTypes != STATIC)
+        {
+            c->_body1->RigidBodyData._linearConstraintForce += f1.head<3>();
+            c->_body1->RigidBodyData._angularConstraintForce += f1.tail<3>();
+        }
+    }
+}
+
+
+
+
+
+
 
 
 
--- include/engine/PhysicsWorld.h
+++ include/engine/PhysicsWorld.h
@@ -2,11 +2,18 @@
 #define CONTACT_PHYSICSWORLD_H
 
 #include "body/RigidBody.h"
+#include "collision/CollisionDetect.h"
 
 #include <Eigen/Dense>
 #include <memory>
 #include <utility>
 #include <vector>
+
+namespace Eigen
+{
+    typedef Matrix<float, 6, 1, ColMajor> Vector6f;
+    typedef Matrix<float, 4, 3, ColMajor> KinematicMap;
+}
 
 class PhysicsWorld
 {
@@ -19,14 +26,12 @@ public:
     void step(float dt);
     void clear();
 
-
-
 public:
     // Callbacks
     typedef std::function<void(std::vector<RigidBody*>&)> PreStepFunc;
     typedef std::function<void()> ResetFunc;
-    void setPreStepFunc(PreStepFunc _func) { m_preStepFunc = std::move(_func); }
-    void setResetFunc(ResetFunc _func) { m_resetFunc = std::move(_func); }
+    void setPreStepFunc(PreStepFunc _func) { _preStepFunc = std::move(_func); }
+    void setResetFunc(ResetFunc _func) { _resetFunc = std::move(_func); }
 
 public:
     /// 这部分属性在Contact.h中也有定义,如果那边没有用到的话，建议把那边的删除
@@ -39,15 +44,15 @@ public:
 
 private:
     void computeInertias(); /// 计算所有刚体的惯性矩阵, 这个函数也会更新旋转矩阵
-    void calcConstraintForces(float dt); /// 计算约束力
+    void calcConstraintForces(float dt) const; /// 计算约束力
 
 public:
     std::vector<RigidBody*> _bodies;
-    //std::unique_ptr<CollisionDetect> _collisionDetect;
+    std::unique_ptr<CollisionDetect> _collisionDetect;
     //Solver* _solver;
 
-    PreStepFunc m_preStepFunc;      /// 用于在每一步模拟之前调用的函数
-    ResetFunc m_resetFunc;          /// 用于重置系统的函数
+    PreStepFunc _preStepFunc;      /// 用于在每一步模拟之前调用的函数
+    ResetFunc _resetFunc;          /// 用于重置系统的函数
 };
 
 template<typename ScalarType, int Options>
